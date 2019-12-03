f84acf21f17e3768fd0d4862f53e2b85cdc8154f
Maya Keeley <maya.keeley@Nology.local>
2019-12-03 16:03:28 +0000

    responsive

--- src/static/data/data.js
+++ src/static/data/data.js
@@ -9,7 +9,7 @@ const projects = [
   {
     projectName: "In Collaboration",
     image: inCollab,
-    url: "https://in-collaboration-85912.web.app/login",
+    url: "https://incollaboration.nology.io",
     techStack: "SCSS, React, Reach Router, Firebase, Auth, Firebase Functions",
     desc:
       "Client project: I worked as part of a team of nine on a client project as part of the _nology training course. During this time we used agile principles, working in sprints with daily standups and weekly retrospectives. The concept was to create a platform for the In Collaboration community to connect on and share ideas and projects. We implemented the use of trello, user stories and paired programming. I worked in a variety of roles, running a retrospective, leading meetings with the client and on the code itself. I, and another team member, taught ourselves how to use firebase functions to send notifications to a mailing list when a post is created on the app."
@@ -17,7 +17,7 @@ const projects = [
   {
     projectName: "Minesweeper",
     image: minesweeper,
-    url: "https://minesweeper.nology.io",
+    url: "https://minesweeper.mayakeeley.com",
     techStack: "HTML, CSS, Javascript",
     desc:
       "_nology brief: Build a program which generates and visualises the minesweeper (uncovered) game field. Input: 3 parameters – number of rows, number of columns, number of mines. Rules: each field is either empty, mine or number (1-8). Generate a rectangular field based on input number of rows and columns. Place mines randomly in the field – there can be only one mine in each field. Calculate the number clues and display the field. Output: simple visualisation of uncovered game board."
